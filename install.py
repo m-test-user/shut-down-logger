93718363f5f962dbf7145bd6e08707f8a842961f
m-test-user <m.src.drop@gmail.com>
2020-11-14 21:29:07 +0800

    done

--- install.py
+++ install.py
@@ -1,15 +1,15 @@
-with open('.envtest', 'w') as file_dot_env:
+if input('If <.env> exists, it will be rewrite (y/n)? ') == 'y':
 
-    secrets = [
-        'TWILIO_ACCOUNT_SID = ',
-        'TWILIO_AUTH_TOKEN = ',
-        'URL = ',
-        'body_message = ',
-        'from_number = +',
-        'to_number = +'
-        ]
+    with open('.env', 'w') as file_dot_env:
 
-    if input('If <.env> exists, it will be rewrite (y/n)? ') == 'y':
+        secrets = [
+            'TWILIO_ACCOUNT_SID = ',
+            'TWILIO_AUTH_TOKEN = ',
+            'URL = ',
+            'body_message = ',
+            'from_number = +',
+            'to_number = +'
+            ]
 
         for i in secrets:
             file_dot_env.write(f'{i}{input(i)}\n')
